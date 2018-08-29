c0721636af4b225d61f946d984effa805e04ef9c
Clark Chambers <clarkchambers94@gmail.com>
2018-08-28 20:59:41 -0500

    Removed sublime workspace:

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ var express = require('express');
 var app = express();
 
 // ARBITRARY COMMENT FOR TEST
-app.get('/', function (req, res) {
+app.get('/',  (req, res) => {
     res.send('Hello World!');
 });
 
-app.listen(3000, funct;i;on;;;;;; () {
+app.listen(3000, () => {
     console.log('Example app listening on port 3000!');
 });
