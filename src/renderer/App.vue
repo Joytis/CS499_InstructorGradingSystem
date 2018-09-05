16ca6dc606a116c5c48d81fc7d028cb33d77eae2
ChrisMc2 <scm0021@uah.edu>
2018-09-04 20:29:48 -0500

    Initial Commit

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'instructor_grading_system'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
---
+++ src/renderer/components/ContentBodyMock.vue
@@ -0,0 +1,21 @@
+<template>
+  <b-tabs size="is-medium" position="is-centered" v-model="activeTab">
+    <b-tab-item label="Unaswered" icon="google-photos"></b-tab-item>
+    <b-tab-item label="Most Asked" icon="library-music"></b-tab-item>
+    <b-tab-item label="FAQ" icon="video"></b-tab-item>
+  </b-tabs>
+</template>
+
+<script>
+export default {
+  name: 'ContentBodyMock',
+  components: {},
+  data () {
+    return {}
+  },
+  methods: {}
+}
+</script>
+
+
+  
