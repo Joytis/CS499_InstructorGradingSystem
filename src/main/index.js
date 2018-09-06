2f3f5a25c48dcaa165e60e248383536c63c44e5d
Joytis <clarkchambers94@gmail.com>
2018-09-05 22:26:07 -0500

    Cleaned up merge and got database connecting with sequelize. But it's not playing nice with electron. need to figure that one out now.

---
+++ src/main/index.js
@@ -0,0 +1,84 @@
+'use strict'
+
+import { app, BrowserWindow } from 'electron'
+var logger = require('../config/winston.js')
+var db = require('../database')
+
+/**
+ * Set `__static` path to static files in production
+ * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-static-assets.html
+ */
+if (process.env.NODE_ENV !== 'development') {
+  global.__static = require('path').join(__dirname, '/static').replace(/\\/g, '\\\\')
+}
+
+db.sequelize
+  .authenticate()
+  .then(async () => {
+    logger.info('Connection has been established successfully.')
+    // Force a sync
+    await db.student.sync({force: true})
+    await db.student.create({
+      firstName: 'John',
+      lastName: 'Hancock'
+    })
+  })
+  .catch(err => {
+    logger.error('Unable to connect to the database:', err)
+  })
+
+let mainWindow
+const winURL = process.env.NODE_ENV === 'development'
+  ? `http://localhost:9080`
+  : `file://${__dirname}/index.html`
+
+function createWindow () {
+  /**
+   * Initial window options
+   */
+  mainWindow = new BrowserWindow({
+    height: 563,
+    useContentSize: true,
+    width: 1000
+  })
+
+  mainWindow.loadURL(winURL)
+
+  mainWindow.on('closed', () => {
+    mainWindow = null
+  })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
+
+/**
+ * Auto Updater
+ *
+ * Uncomment the following code below and install `electron-updater` to
+ * support auto updating. Code Signing with a valid certificate is required.
+ * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-electron-builder.html#auto-updating
+ */
+
+/*
+import { autoUpdater } from 'electron-updater'
+
+autoUpdater.on('update-downloaded', () => {
+  autoUpdater.quitAndInstall()
+})
+
+app.on('ready', () => {
+  if (process.env.NODE_ENV === 'production') autoUpdater.checkForUpdates()
+})
+ */
