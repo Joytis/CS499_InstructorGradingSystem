ac7080515d3fa91e9e090a5f1012e09be8b6c18c
mlw0022 <mlw0022@uah.edu>
2018-10-04 16:38:40 -0500

    signin modal, in contentbodymock

--- src/renderer/components/ContentBodyMock.vue
+++ src/renderer/components/ContentBodyMock.vue
@@ -1,23 +1,68 @@
 <template>
-  <b-tabs size="is-medium" position="is-centered" v-model="activeTab">
-    <b-tab-item label="Unaswered" icon="google-photos"></b-tab-item>
-    <b-tab-item label="Most Asked" icon="library-music"></b-tab-item>
-    <b-tab-item label="FAQ" icon="video"></b-tab-item>
-  </b-tabs>
-</template>
+  <section>
+    <button class="button is-primary is-medium"
+      @click="isComponentModalActive = true">
+      Sign In
+    </button>
 
+    <b-modal :active.sync="isComponentModalActive" has-modal-card>
+      <modal-form v-bind="formProps"></modal-form>
+    </b-modal>
+  </section>
+</template>
+  
 <script>
+const ModalForm = {
+  props: ['email', 'password'],
+  template: `
+            <form action="">
+                <div class="modal-card" style="width: auto">
+                    <header class="modal-card-head">
+                        <p class="modal-card-title">Login</p>
+                    </header>
+                    <section class="modal-card-body">
+                        <b-field label="Email">
+                            <b-input
+                                type="email"
+                                :value="email"
+                                placeholder="Your email"
+                                required>
+                            </b-input>
+                        </b-field>
+
+                        <b-field label="Password">
+                            <b-input
+                                type="password"
+                                :value="password"
+                                password-reveal
+                                placeholder="Your password"
+                                required>
+                            </b-input>
+                        </b-field>
+
+                        <b-checkbox>Remember me</b-checkbox>
+                    </section>
+                    <footer class="modal-card-foot">
+                        <button class="button" type="button" @click="$parent.close()">Close</button>
+                        <button class="button is-primary">Login</button>
+                    </footer>
+                </div>
+            </form>
+        `
+}
+
 export default {
-  name: 'ContentBodyMock',
-  components: {},
+  components: {
+    ModalForm
+  },
+  props: ['isComponentModalActive'],
   data () {
     return {
-      activeTab: null
+      formProps: {
+        email: '',
+        password: ''
+      }
     }
-  },
-  methods: {}
+  }
 }
 </script>
-
-
-  
