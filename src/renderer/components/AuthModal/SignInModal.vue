8eca33ef66f51c61627ef2abf7cfe7f2698d7b6e
mlw0022 <mlw0022@uah.edu>
2018-10-07 16:06:52 -0500

    created sign in/up modal forms

---
+++ src/renderer/components/AuthModal/SignInModal.vue
@@ -0,0 +1,50 @@
+<template>
+            <form action="">
+                <div class="modal-card" style="width: auto">
+                    <header class="modal-card-head">
+                        <p class="modal-card-title">Login</p>
+                    </header>
+                    <section class="modal-card-body">
+                        <b-field label="Email">
+                            <b-input
+                                type="email"
+                                :value="email"
+                                placeholder="uah@email.com"
+                                required>
+                            </b-input>
+                        </b-field>
+
+                        <b-field label="Password">
+                            <b-input
+                                type="password"
+                                :value="password"
+                                password-reveal
+                                placeholder="password"
+                                required>
+                            </b-input>
+                        </b-field>
+
+                    </section>
+                    <footer class="modal-card-foot">
+                        <button class="button" type="button" @click="$parent.close()">Close</button>
+                        <button class="button is-primary" @click="$parent.close()">Login</button>
+                    </footer>
+                </div>
+            </form>
+</template>
+
+
+
+<script>
+export default {
+  components: {
+  },
+  props: [],
+  data() {
+    return {
+      email: '',
+      password: '',
+    };
+  },
+};
+</script>
