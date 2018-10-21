bf1faca6b8b9108d4cc96633f8a9f86255f38b3e
Joytis <clarkchambers94@gmail.com>
2018-10-21 12:38:02 -0500

    Created a class that handles all API routes currently. Just point it at a host and a route, and start sending it some spicy objects. A simple test script was created in the scratch folder.

---
+++ scratch/clarkTestingLol.js
@@ -0,0 +1,48 @@
+const winston = require('winston');
+const { SimpleCrud } = require('../middleware');
+
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
+const logger = winston.createLogger({
+  transports: [
+    new winston.transports.Console({
+      format: winston.format.combine(winston.format.colorize(), winston.format.simple()),
+    }),
+  ],
+  exitOnError: false,
+});
+
+const host = 'https://localhost:3000';
+
+const AccountCrud = new SimpleCrud(host, '/instructor/account');
+const LoginCrud = new SimpleCrud(host, '/instructor/login');
+const LogoutCrud = new SimpleCrud(host, '/instructor/logout');
+
+async function simpleTest() {
+  try {
+    logger.info('Testing account creation');
+    let response = await AccountCrud.post({
+      username: 'Coleman',
+      password: 'colemancs499',
+      firstName: 'Professor',
+      lastName: 'Coleman',
+      email: 'coleman@coleman.col',
+    });
+    logger.info(JSON.stringify(response, null, 2));
+
+    logger.info('Testing account login');
+    response = await LoginCrud.post({
+      username: 'Coleman',
+      password: 'colemancs499',
+    });
+    logger.info(JSON.stringify(response, null, 2));
+
+    logger.info('Testing account logout');
+    response = await LogoutCrud.post({});
+    logger.info(JSON.stringify(response, null, 2));
+  } catch (err) {
+    logger.error(err);
+  }
+}
+
+simpleTest();
