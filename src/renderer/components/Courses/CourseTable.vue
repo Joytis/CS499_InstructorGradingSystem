ba8e484aadc2e5b4b53ee132974e2b81de344219
Joytis <clarkchambers94@gmail.com>
2018-10-25 16:35:13 -0500

    Psyche now we're good.

--- src/renderer/components/Courses/CourseTable.vue
+++ src/renderer/components/Courses/CourseTable.vue
@@ -1,69 +1,66 @@
 <template>
-  <div>
+  <b-table
+      :data="datamock"
+      paginated
+      per-page="5"
+      detailed
+      detail-key="CourseId"
+  >
+
+    <template slot-scope="props">
+      <b-table-column field="CourseId" label="Course ID" width="130" sortable>
+        {{ props.row.CourseId }}
+      </b-table-column>
+      <b-table-column field="CourseName" label="Course Name" sortable>
+        {{ props.row.CourseName }}
+      </b-table-column>
+
+      <b-table-column field="CourseSections" label="Number of Sections" numeric>
+        {{ props.row.CourseSections }}
+      </b-table-column>
+
+      <b-table-column field="CourseAvg" label="Course Average" sortable>
+        {{ props.row.CourseAvg }}
+      </b-table-column>
+      <b-table-column label="Course Page">
+        <button class="button is-warning is-small">
+          <router-link :to="'courses/' + props.row.CourseId">
+            <b-icon type="is-accent" icon="expand-all">
+            </b-icon>
+          </router-link>
+        </button>
+      </b-table-column>
+
+    </template>
+
+    <template slot="detail" slot-scope="props">
       <b-table
-          :data="datamock"
-          paginated
-          per-page="5"
-          detailed
-          detail-key="CourseId"
+        :data=getSection(props.row.CourseId)
       >
+        <template slot-scope="props">
+          <b-table-column field="SectionName" label="Section Name" width="180" sortable>
+            {{ props.row.SectionName }}
+          </b-table-column>
 
-          <template slot-scope="props">
-            <b-table-column field="CourseId" label="Course ID" width="130" sortable>
-              {{ props.row.CourseId }}
-            </b-table-column>
-            <b-table-column field="CourseName" label="Course Name" sortable>
-              {{ props.row.CourseName }}
-            </b-table-column>
+          <b-table-column field="SectionTime" label="Section Time" sortable>
+            {{ props.row.SectionTime }}
+          </b-table-column>
 
-            <b-table-column field="CourseSections" label="Number of Sections" numeric>
-              {{ props.row.CourseSections }}
-            </b-table-column>
+          <b-table-column field="NumStudents" label="Number of Students" numeric>
+            {{ props.row.NumStudents }}
+          </b-table-column>
 
-            <b-table-column field="CourseAvg" label="Course Average" sortable>
-              {{ props.row.CourseAvg }}
-            </b-table-column>
-            <b-table-column label="Course Page">
-              <button class="button is-warning is-small">
-                <router-link :to="'courses/' + props.row.CourseId">
-                  <b-icon type="is-accent" icon="expand-all">
-                  </b-icon>
-                </router-link>
-              </button>
-            </b-table-column>
+          <b-table-column field="SectionAvg" label="Section Average" sortable>
+            {{ props.row.SectionAvg }}
+          </b-table-column>
+          <b-table-column label="Section Page">
+            <button class="button is-warning is-small">+</button>
+          </b-table-column>
+        </template>
 
-          </template>
-
-          <template slot="detail" slot-scope="props">
-            <b-table
-              :data=getSection(props.row.CourseId)
-            >
-              <template slot-scope="props">
-                <b-table-column field="SectionName" label="Section Name" width="180" sortable>
-                  {{ props.row.SectionName }}
-                </b-table-column>
-
-                <b-table-column field="SectionTime" label="Section Time" sortable>
-                  {{ props.row.SectionTime }}
-                </b-table-column>
-
-                <b-table-column field="NumStudents" label="Number of Students" numeric>
-                  {{ props.row.NumStudents }}
-                </b-table-column>
-
-                <b-table-column field="SectionAvg" label="Section Average" sortable>
-                  {{ props.row.SectionAvg }}
-                </b-table-column>
-                <b-table-column label="Section Page">
-                  <button class="button is-warning is-small">+</button>
-                </b-table-column>
-              </template>
-
-            </b-table>
-          </template>
       </b-table>
-      <button @click="fetchData">WOW :D</button>
-  </div>
+    </template>
+  </b-table>
 </template>
 
 <script>
