dd30cff7f4b0a1f87ff87acdbb7dae5adcd50d0a
mlw0022 <mlw0022@uah.edu>
2018-10-30 14:41:31 -0500

    fixed course page and removed dead code

--- src/renderer/components/Courses/CoursePage.vue
+++ src/renderer/components/Courses/CoursePage.vue
@@ -13,44 +13,6 @@
         </div>
       </nav>
     </div>
-    Course At a Glance:
-    <div class="box">
-      <nav class="level">
-        <div class="level-item has-text-centered">
-          <div>
-            <div class="columns">
-              <p class="heading">Mean</p>
-              <p class="title">70</p>
-            </div>
-          </div>
-        </div>
-        <div class="level-item has-text-centered">
-          <div>
-            <div class="columns">
-              <p class="heading">Median</p>
-              <p class="title">70</p>
-            </div>
-          </div>
-        </div>
-        <div class="level-item has-text-centered">
-          <div>
-            <div class="columns">
-              <p class="heading">Mode</p>
-              <p class="title">70</p>
-            </div>
-          </div>
-        </div>
-        <div class="level-item has-text-centered">
-          <div>
-            <div class="columns">
-              <p class="heading">Std. Dev</p>
-              <p class="title">70</p>
-            </div>
-          </div>
-        </div>
-      </nav>
-    </div>
-    <br>
     Students of Concern:
     <b-table
         :data="datamock"
@@ -79,9 +41,6 @@
 <script>
 import data from './CourseListDataMock';
 
-// Hello Course Page
-// {{ $route.path }}
-// {{ $route.params }}
 export default {
   name: 'CoursePage',
   beforeCreate() {
@@ -110,50 +69,4 @@ export default {
     },
   },
 };
-
-// <b-table
-//     :data=getdata($route.params.id)
-//     detailed
-//   >
-//     <template slot-scope="props">
-//         <b-table-column field="CourseId" label="Course ID" width="130" sortable>
-//           {{ props.row.CourseId }}
-//         </b-table-column>
-//         <b-table-column field="CourseName" label="Course Name" sortable>
-//           {{ props.row.CourseName }}
-//         </b-table-column>
-//         <b-table-column field="CourseSections" label="Number of Sections" numeric>
-//           {{ props.row.CourseSections }}
-//         </b-table-column>
-//         <b-table-column field="CourseAvg" label="Course Average" sortable>
-//           {{ props.row.CourseAvg }}
-//         </b-table-column>
-//     </template>
-
-//     <template slot="detail" slot-scope="props">
-//       <b-table
-//         :data=getSection(props.row.CourseId)
-//       >
-//         <template slot-scope="props">
-//                 <b-table-column field="SectionName" label="Section Name" width="180" sortable>
-//                   {{ props.row.SectionName }}
-//                 </b-table-column>
-
-//                 <b-table-column field="SectionTime" label="Section Time" sortable>
-//                   {{ props.row.SectionTime }}
-//                 </b-table-column>
-
-//                 <b-table-column field="NumStudents" label="Number of Students" numeric>
-//                   {{ props.row.NumStudents }}
-//                 </b-table-column>
-
-//                 <b-table-column field="SectionAvg" label="Section Average" sortable>
-//                   {{ props.row.SectionAvg }}
-//                 </b-table-column>
-//         </template>
-//       </b-table>
-
-//     </template>
-
-//   </b-table>
 </script>
