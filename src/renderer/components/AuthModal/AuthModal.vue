43e0cd5db7fa07a7276caf016e31f2335605b796
Joytis <clarkchambers94@gmail.com>
2018-11-12 14:50:17 -0600

    Got the login screen looking acceptable! Phew. css is a mess.

--- src/renderer/components/AuthModal/AuthModal.vue
+++ src/renderer/components/AuthModal/AuthModal.vue
@@ -1,33 +1,42 @@
 <template>
-  <div class="columns">
-    <div class="column is-half primary">
-    </div>
-    <div class="column is-half">
-      <button class="button is-primary is-medium" @click="modalForm = 'signin'; isModalActive = true">
-        Sign In
-      </button>
+  <section class="section">
+    <navbar-login></navbar-login>
+    <div class="columns">
+      <div class="column is-half primary">
+        <div class="logo">
+          <img src="../../../../static/gbpp_logo.png" width="200" height="auto">
+          <div>Gradebook++</div>
+        </div>
+      </div>
+      <div class="column is-half login-buttons">
+        <button class="button is-primary is-large" @click="modalForm = 'signin'; isModalActive = true">
+          Sign In
+        </button>
 
-      <button class="button is-primary is-medium" @click="modalForm = 'signup'; isModalActive = true">
-        Sign Up
-      </button>
+        <button class="button is-primary is-large" @click="modalForm = 'signup'; isModalActive = true">
+          Sign Up
+        </button>
 
-      <b-modal :active.sync="isModalActive" has-modal-card>
-        <creation-modal-form v-if="modalForm === 'signin'" :inputs="signInModalInputs"/>
-        <creation-modal-form v-else-if="modalForm === 'signup'" :inputs="signUpModalInputs"/>
-      </b-modal>
+        <b-modal :active.sync="isModalActive" has-modal-card>
+          <creation-modal-form v-if="modalForm === 'signin'" :inputs="signInModalInputs"/>
+          <creation-modal-form v-else-if="modalForm === 'signup'" :inputs="signUpModalInputs"/>
+        </b-modal>
+      </div>
     </div>
-  </div>
+  </section>
 </template>
   
 <script>
 /* eslint-disable no-alert */
 import CreationModalForm from '../CreationModal.vue';
 import InstructorModalInputs from '../InstructorModalInputs';
+import NavbarLogin from '../NavbarLogin.vue';
 
 export default {
   name: 'AuthModal',
   components: {
     CreationModalForm,
+    NavbarLogin,
   },
   data() {
     return {
@@ -39,5 +48,30 @@ export default {
   },
   methods: {},
 };
-</script>
+</script>/
+
+<style lang="scss">
+@import "../../App";
+
+.primary {
+  background-color: nth(map-get($colors, primary), 1);
+  height: 80vh;
+  margin-top: 8vh;
+  overflow: hidden;
+}
+
+.login-buttons {
+  text-align: center;
+  margin-top: 60vh;
+}
+
+.logo {
+  color: white;
+  font-size: 5em;
+  text-align: center;
+  margin-top: 30vh;
+}
+
+</style>
+
 
