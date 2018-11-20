804dcf71458b3f6a8812db821391914732c13121
mlw0022 <mlw0022@uah.edu>
2018-11-20 14:21:29 -0600

    got rid of course page link and added section grading scale visual

--- src/renderer/components/Sections/WhatIfModals/stuffNeededforModal.vue
+++ src/renderer/components/Sections/WhatIfModals/stuffNeededforModal.vue
@@ -20,55 +20,55 @@
 </template>
 
 <script>
-import AssCatCreateModalForm from './WhatIfModals/AssCatCreate.vue';
-import AssCatUpdateModalForm from './WhatIfModals/AssCatUpdate.vue';
-import AssUpdateModalForm from './WhatIfModals/AssUpdate.vue';
+// import AssCatCreateModalForm from './WhatIfModals/AssCatCreate.vue';
+// import AssCatUpdateModalForm from './WhatIfModals/AssCatUpdate.vue';
+// import AssUpdateModalForm from './WhatIfModals/AssUpdate.vue';
 
 export default {
-    components: {
-        AssCatUpdateModalForm,
-        AssCatCreateModalForm,
-        AssUpdateModalForm,
-    },
-    data() {
-        return {
-           isCreationModalActive: false,
-           isUpdateModalActive: false,
-           isAssUpdateModalActive: false,
-        }
-    },
-    assCatTestInputs: {
-        templates: {
-          name: { label: 'Name', type: 'input', placeholder: 'Category Name' },
-          weight: {
-            label: 'Weight', type: 'input', subtype: 'number', placeholder: 0.0, step: 0.01, max: 1,
-          },
-          lowestGradesDropped: {
-            label: 'Lowest Grades Dropped', type: 'input', subtype: 'number', placeholder: 0,
-          },
-        },
+  components: {
+    // AssCatUpdateModalForm,
+    // AssCatCreateModalForm,
+    // AssUpdateModalForm,
+  },
+  data() {
+    return {
+      isCreationModalActive: false,
+      isUpdateModalActive: false,
+      isAssUpdateModalActive: false,
+    };
+  },
+  assCatTestInputs: {
+    templates: {
+      name: { label: 'Name', type: 'input', placeholder: 'Category Name' },
+      weight: {
+        label: 'Weight', type: 'input', subtype: 'number', placeholder: 0.0, step: 0.01, max: 1,
       },
-      assUpdateTestInputs: {
-        templates: {
-          assignmentCategoryId: {
-            label: 'Assignment Category',
-            type: 'dropdown',
-            getData: null,
-            value: 'id',
-            key: 'id,',
-            display: option => option.name,
-          },
-          name: {
-            label: 'Name', type: 'input', placeholder: 'Assignment name',
-          },
-          totalPoints: {
-            label: 'Total Points', type: 'input', subtype: 'number', placeholder: 100,
-          },
-          dueDate: {
-            label: 'Due Date', type: 'datepicker', placeholder: Date.now(),
-          },
-        },
-      },    
-}
+      lowestGradesDropped: {
+        label: 'Lowest Grades Dropped', type: 'input', subtype: 'number', placeholder: 0,
+      },
+    },
+  },
+  assUpdateTestInputs: {
+    templates: {
+      assignmentCategoryId: {
+        label: 'Assignment Category',
+        type: 'dropdown',
+        getData: null,
+        value: 'id',
+        key: 'id,',
+        display: option => option.name,
+      },
+      name: {
+        label: 'Name', type: 'input', placeholder: 'Assignment name',
+      },
+      totalPoints: {
+        label: 'Total Points', type: 'input', subtype: 'number', placeholder: 100,
+      },
+      dueDate: {
+        label: 'Due Date', type: 'datepicker', placeholder: Date.now(),
+      },
+    },
+  },
+};
 </script>
 
