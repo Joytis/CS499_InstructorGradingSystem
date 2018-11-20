28fb5774a7734d2780f10f97678b1d7cf013d001
mlw0022 <mlw0022@uah.edu>
2018-11-20 14:07:17 -0600

    modals for what if

---
+++ src/renderer/components/Sections/WhatIfModals/AssCatCreate.vue
@@ -0,0 +1,138 @@
+<template>
+  <div class="modal-card crud-modal" style="width: auto">
+    <header class="modal-card-head">
+      <p class="modal-card-title"> {{ this.title }} </p>
+    </header>
+    <section class="modal-card-body">
+      <div v-if="state === 'loading'">
+        <atom-spinner :animation-duration="1000" :size="200" :color="'#cd5bef'"/>
+      </div>
+      <div v-else-if="state === 'error'">
+        <div> {{ error }} </div>
+      </div>
+      <div v-else-if="state === 'success'">
+        Success!
+      </div>
+      <div v-else v-for="(field, key) in inputs.templates">
+        <b-field :label="field.label">
+          <div v-if="field.type === 'input'">
+            <b-input 
+              v-model="staged[key]" 
+              :type="field.subtype" 
+              :step="field.step || 1" 
+              :min="field.min || 0" 
+              :placeholder="(field.placeholder) ? field.placeholder : ''" 
+              required/>
+          </div>
+          <div v-else-if="field.type === 'datepicker'">
+            <b-datepicker
+              v-model="staged[key]"
+              :placeholder="field.placeholder"
+              icon="calendar-today" 
+              editable 
+              inline
+              required />
+          </div>
+          <div v-else-if="field.type === 'password'">
+            <b-input 
+              type="password" 
+              v-model="staged[key]" 
+              password-reveal 
+              :placeholder="field.placeholder" 
+              required/>
+          </div>
+          <div v-else>
+            NO VALID TYPE GIVEN
+          </div>
+        </b-field>
+      </div>
+    </section>
+    <footer class="modal-card-foot">
+      <div v-if="state === 'main'">
+        <button class="button" type="button" @click="$parent.close()">Close</button>
+        <button class="button is-primary"v-text="primaryButtonText()" @click="attemptDatabaseCreate"/>
+      </div>
+    </footer>
+  </div>
+</template>
+
+
+
+<script>
+/* eslint-disable no-console */
+import { AtomSpinner } from 'epic-spinners';
+import { ParseError } from '../../../../../middleware';
+
+export default {
+  name: 'CreateAssCatForm',
+  components: {
+    AtomSpinner,
+  },
+  props: {
+    title: String,
+    inputs: Object,
+  },
+  data() {
+    return {
+      error: undefined,
+      state: 'main',
+      staged: {},
+      searchString: '',
+      checkRows: [],
+    };
+  },
+
+  created() {
+    Object.keys(this.inputs.templates).forEach((key) => { this.staged[key] = ''; });
+  },
+
+  methods: {
+    primaryButtonText() {
+      return ((this.inputs.primaryText !== undefined) ? this.inputs.primaryText : 'Create');
+    },
+
+    async attemptDatabaseCreate() {
+      try {
+        // If we have a preCreate defined, mutate staged with its staged.
+        if (this.inputs.preCreate !== undefined) {
+          this.staged = await this.inputs.preCreate(this.staged);
+        }
+        // Display loading
+        this.state = 'loading';
+        // Wait for term creation
+        const result = (await this.inputs.crudTarget.post(this.staged)).data;
+        // wait for two seconds then close window.
+        this.state = 'success';
+        // if we want to do something afterwards, do it here!
+        if (this.inputs.postCreate !== undefined) {
+          await this.inputs.postCreate(result);
+        }
+      } catch (err) {
+        this.state = 'error';
+        console.log(err);
+        this.error = ParseError(err);
+        // DISPLAY ERROR MODAL?
+      }
+    },
+    filteredData(allStudents) {
+      return allStudents.filter((student) => (
+        student.firstName.toLowerCase().includes(this.searchString.toLowerCase())
+        || student.lastName.toLowerCase().includes(this.searchString.toLowerCase())));
+    },
+  },
+};
+</script>
+
+<style>
+.crud-modal {
+  color: black;
+}
+
+.modal .animation-content .modal-card {
+  overflow: visible !important;
+}
+
+.modal-card-body {
+  overflow: visible !important;
+}
+</style>
---
+++ src/renderer/components/Sections/WhatIfModals/stuffNeededforModal.vue
@@ -0,0 +1,74 @@
+<template>
+     <button class="button" @click="isCreationModalActive = true">
+      AssCatCreate
+    </button>
+    <button class="button" @click="isUpdateModalActive = true">
+      AssCatUpdate
+    </button>
+    <button class="button" @click="isAssUpdateModalActive = true">
+      AssUpdate
+    </button>
+    <b-modal :active.sync="isCreationModalActive" has-modal-card>
+      <ass-cat-create-modal-form :inputs="assCatTestInputs"></ass-cat-create-modal-form>
+    </b-modal>
+    <b-modal :active.sync="isUpdateModalActive" has-modal-card>
+      <ass-cat-update-modal-form :inputs="assCatTestInputs" :target="selectedAssignmentCategory"></ass-cat-update-modal-form>
+    </b-modal>
+    <b-modal :active.sync="isAssUpdateModalActive" has-modal-card>
+      <ass-update-modal-form :inputs="assUpdateTestInputs" :target="selectedAssignment"></ass-update-modal-form>
+    </b-modal>
+</template>
+
+<script>
+import AssCatCreateModalForm from './WhatIfModals/AssCatCreate.vue';
+import AssCatUpdateModalForm from './WhatIfModals/AssCatUpdate.vue';
+import AssUpdateModalForm from './WhatIfModals/AssUpdate.vue';
+
+export default {
+    components: {
+        AssCatUpdateModalForm,
+        AssCatCreateModalForm,
+        AssUpdateModalForm,
+    },
+    data() {
+        return {
+           isCreationModalActive: false,
+           isUpdateModalActive: false,
+           isAssUpdateModalActive: false,
+        }
+    },
+    assCatTestInputs: {
+        templates: {
+          name: { label: 'Name', type: 'input', placeholder: 'Category Name' },
+          weight: {
+            label: 'Weight', type: 'input', subtype: 'number', placeholder: 0.0, step: 0.01, max: 1,
+          },
+          lowestGradesDropped: {
+            label: 'Lowest Grades Dropped', type: 'input', subtype: 'number', placeholder: 0,
+          },
+        },
+      },
+      assUpdateTestInputs: {
+        templates: {
+          assignmentCategoryId: {
+            label: 'Assignment Category',
+            type: 'dropdown',
+            getData: null,
+            value: 'id',
+            key: 'id,',
+            display: option => option.name,
+          },
+          name: {
+            label: 'Name', type: 'input', placeholder: 'Assignment name',
+          },
+          totalPoints: {
+            label: 'Total Points', type: 'input', subtype: 'number', placeholder: 100,
+          },
+          dueDate: {
+            label: 'Due Date', type: 'datepicker', placeholder: Date.now(),
+          },
+        },
+      },    
+}
+</script>
+
